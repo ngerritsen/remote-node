1aa66e952aaa49066b518061674a1e455900ae2d
Niels Gerritsen <n.c.gerritsen@gmail.com>
2017-06-28 22:02:40 +0200

    Add tab

--- client/main.js
+++ client/main.js
@@ -1,4 +1,6 @@
-const resultErrorClassName = 'results--error';
+const RESULT_ERROR_CLASS_NAME = 'results--error';
+const TAB_KEY_CODE = 9;
+const TAB_SIZE = 2;
 
 window.addEventListener('DOMContentLoaded', () => {
   const formEl = document.querySelector('.js-form');
@@ -10,6 +12,13 @@ window.addEventListener('DOMContentLoaded', () => {
     runCode(inputEl.value);
   });
 
+  inputEl.addEventListener('keydown', (e) => {
+    if (event.keyCode === TAB_KEY_CODE) {
+      e.preventDefault();
+      handleTab();
+    }
+  });
+
   function runCode(code) {
     fetch('/run', { method: 'POST', body: code })
       .then(res => res.json())
@@ -17,16 +26,35 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   function setResult(result) {
-    const isInErrorState = resultsEl.classList.contains(resultErrorClassName);
+    const isInErrorState = resultsEl.classList.contains(RESULT_ERROR_CLASS_NAME);
 
     if (result.success && isInErrorState) {
-      resultsEl.classList.remove(resultErrorClassName)
+      resultsEl.classList.remove(RESULT_ERROR_CLASS_NAME)
     }
 
     if (!result.success && !isInErrorState) {
-      resultsEl.classList.add(resultErrorClassName)
+      resultsEl.classList.add(RESULT_ERROR_CLASS_NAME)
     }
 
     resultsEl.textContent = result.data;
+  }
+
+  function handleTab() {
+    const { value, selectionStart: start } = inputEl;
+    const newCursorPosition = start + TAB_SIZE;
+
+    inputEl.value = value.substring(0, start) + generateSpaces() + value.substring(start);
+    inputEl.selectionStart = newCursorPosition;
+    inputEl.selectionEnd = newCursorPosition;
+  }
+
+  function generateSpaces() {
+    let spaces = '';
+
+    for (let i = 0; i < TAB_SIZE; i++) {
+      spaces += ' ';
+    }
+
+    return spaces;
   }
 });
